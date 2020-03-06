5562b08d7cea322652375a8a66c75e5a782291d4
Kendy Nguyen <kendyisnguyen@gmail.com>
2020-03-06 11:34:17 -0800

    add menu and burger code in nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,6 +4,114 @@ import { css } from "@emotion/core"
 import { colors } from "../styles/theme"
 const linkHoverColor = colors.linkColor
 
+//#region left aligned
+const StyledMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: ${mobileMenuBgColor};
+  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
+  height: 100vh;
+  text-align: left;
+  padding: 1rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: transform 0.3s ease-in-out;
+
+  @media (max-width: 576px) {
+    width: 100%;
+  }
+
+  a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: ${colors.navTextColor};
+    text-decoration: none;
+    transition: color 0.3s linear;
+
+    @media (max-width: 576px) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    &:hover {
+      color: #343078;
+    }
+  }
+`
+
+const Menu = ({ open }) => {
+  return (
+    <StyledMenu open={open}>
+      <a href="/">
+        <span aria-label="projects">Projects</span>
+      </a>
+      <a href="/">
+        <span role="img" aria-label="contact me">
+          Contact Me
+        </span>
+      </a>
+    </StyledMenu>
+  )
+}
+
+const StyledBurger = styled.button`
+  position: absolute;
+  top: 1.5rem;
+  left: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+
+  &:focus {
+    outline: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ open }) => (open ? `${colors.navTextColor}` : "#EFFFFA")};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? "0" : "1")};
+      transform: ${({ open }) => (open ? "translateX(20px)" : "translateX(0)")};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+    }
+  }
+`
+
+const Burger = ({ open, setOpen }) => {
+  return (
+    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  )
+}
+
 const Nav = ({ location }) => {
   // Scroll to the section
   // @param id is the id name of the section (string)
