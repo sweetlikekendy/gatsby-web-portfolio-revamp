d995c3159f1e5686f1172b3c762d57e794ca0529
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-01-22 14:21:04 -0800

    active bg color for scroll top button is a darker orange

--- src/components/back-to-top.js
+++ src/components/back-to-top.js
@@ -18,7 +18,7 @@ const BackToTop = () => {
     <div className="my-4 flex flex-col justify-center items-center mx-auto md:mt-24 md:mx-auto md:mb-8 ">
       <button
         id="button-up"
-        className="to-top-link bg-violet-400 rounded-full h-20 w-20 mb-4 shadow-md flex justify-center items-center cursor-pointer bg-transparent border-none transition-colors focus:outline-none focus:border-gray-700 md:hover:bg-orange-400 md:hover:shadow-lg md:active:bg-orange-300 md:active:shadow-2xl"
+        className="to-top-link bg-violet-400 rounded-full h-20 w-20 mb-4 shadow-md flex justify-center items-center cursor-pointer bg-transparent border-none transition-colors focus:outline-none focus:border-gray-700 md:hover:bg-orange-400 md:hover:shadow-lg md:active:bg-orange-500 md:active:shadow-2xl"
         onClick={handleClick}
       >
         <Arrow
