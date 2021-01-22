d0c56b48560d7a0fc0be09282466c0dc4619940f
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-01-22 15:04:19 -0800

    change max w from 7xl to 6xl

--- src/components/styles/section.js
+++ src/components/styles/section.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Section(props) {
   return (
-    <section className="max-w-7xl mt-12 mx-auto p-4 xl:p-0" {...props}>
+    <section className="max-w-6xl mt-12 mx-auto p-4 xl:p-0" {...props}>
       {props.children}
     </section>
   )
