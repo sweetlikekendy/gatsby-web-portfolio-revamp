d9ffce778bb5a5d9c24ef8512eaa63bda1931eee
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-01-24 14:32:56 -0800

    convert hero to tailwind

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,48 +5,12 @@ import SocialLinks from "./social-links"
 
 const Hero = () => {
   return (
-    <div
-      css={css`
-        z-index: 2;
-        max-width: 800px;
-        margin: -150px auto 210px;
-        background-color: #fff;
-        border: 1px solid ${colors.formBgColor};
-
-        @media screen and (max-width: 800px) {
-          margin: 1rem;
-          /* padding: 1rem; */
-        }
-      `}
-    >
-      <div
-        className="top-about-me"
-        css={css`
-          height: 100%;
-          padding: 2rem 1rem;
-          h2 {
-            text-align: center;
-            letter-spacing: 1.5px;
-            margin-bottom: 1rem;
-          }
-
-          p {
-            padding: 1rem 4rem;
-            line-height: 1.7;
-            letter-spacing: 1px;
-          }
-          @media screen and (max-width: 375px) {
-            h2 {
-              text-align: left;
-            }
-            p {
-              padding: 1rem 0;
-            }
-          }
-        `}
-      >
-        <h2 className="text-2xl font-medium">Hi! My name is Kendy</h2>
-        <p>
+    <div className="z-2 max-w-4xl bg-white border-2 border-blueGray-200 m-4 md:-mt-36 md:mb-52 mx-auto">
+      <div className="h-full py-8 px-4">
+        <h2 className="text-2xl font-medium text-left mb-4 sm:text-center">
+          Hi! My name is Kendy
+        </h2>
+        <p className="py-4 px-0 leading-7 tracking-wider sm:px-16 ">
           I am a web developer based in San Diego, CA. My work experiences come
           from freelancing for small businesses. I've dealt with Shopify stores,
           and building small static sites with a static site generator such as{" "}
@@ -62,28 +26,8 @@ const Hero = () => {
           mentality!
         </p>
       </div>
-      <div
-        className="bottom-about-me"
-        css={css`
-          background-color: ${colors.formBgColor};
-          height: 70px;
-          padding: 1rem;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        `}
-      >
-        <SocialLinks
-          css={css`
-            svg {
-              width: 25px;
-            }
-          `}
-          fill={colors.formBgColor}
-          outline={colors.textPrimaryBodyColor}
-          dimension={parseInt(25)}
-          hoverColor={colors.linkColor}
-        />
+      <div className="h-16 p-4 bg-blueGray-200 flex justify-center items-center">
+        <SocialLinks />
       </div>
     </div>
   )
