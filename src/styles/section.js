b219a487486e86f1fba8c47ede1b6ab8dfbf7eb0
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-01-24 16:40:36 -0800

    forgot to add these to previous commit. move src/components/styles to src/styles

---
+++ src/styles/section.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Section(props) {
+  return (
+    <section className="max-w-6xl mt-12 mx-auto p-4 xl:p-0" {...props}>
+      {props.children}
+    </section>
+  )
+}
