1cca067249be3215e5aa1b8b2ea944f0e4f019d9
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-01-28 13:50:45 -0800

    change project data keys to match project-card

--- src/allProjects.js
+++ src/allProjects.js
@@ -7,29 +7,35 @@ import letMeInImg from "./images/let-me-in.png"
 
 export const allProjects = [
   {
-    name: "Chinatown Community Development Center",
+    title: "Chinatown Community Development Center",
     techStack: ["html5", "css3"],
-    img: ccdcImg,
+    imgSrc: ccdcImg,
+    imgAlt: "",
     description: "Redesign of a careers landing page.",
     demoLink: "https://chinatowncdc.applytojob.com/apply/",
+    type: "Client Work",
   },
   {
-    name: "Nanocraft CBD",
+    title: "Nanocraft CBD",
     techStack: ["shopify", "html5", "css3", "javascript"],
-    img: nanocraftcbdImg,
+    imgSrc: nanocraftcbdImg,
+    imgAlt: "",
     description:
       "Redesigned product landing pages. Added more features to already existing theme",
+    type: "Client Work",
     demoLink: "http://nanocraftcbd.com/",
   },
   {
-    name: "PETLYcbd",
+    title: "PETLYcbd",
     techStack: ["shopify", "html5", "css3", "javascript"],
-    img: petlycbdImg,
+    imgSrc: petlycbdImg,
+    imgAlt: "",
     description: "Added more features to already existing theme",
     demoLink: "http://petlycbd.com/",
+    type: "Client Work",
   },
   {
-    name: "Smart Landscape SD",
+    title: "Smart Landscape SD",
     techStack: [
       "html5",
       "css3",
@@ -39,13 +45,15 @@ export const allProjects = [
       "sanity",
       "netlify",
     ],
-    img: smartlandscapeImg,
+    imgSrc: smartlandscapeImg,
+    imgAlt: "",
     description:
       "Redesign of another landscaping business page. The form on the old website was not working, redesigned to have form on every single page.",
+    type: "Client Work",
     demoLink: "http://smartlandscapesd.com/",
   },
   {
-    name: "Calvine Landscape",
+    title: "Calvine Landscape",
     techStack: [
       "html5",
       "css3",
@@ -55,13 +63,15 @@ export const allProjects = [
       "sanity",
       "netlify",
     ],
-    img: calvinelandscapeImg,
+    imgSrc: calvinelandscapeImg,
+    imgAlt: "",
     description:
       "Redesigned a client's landscaping business page. Using Gatsby as a foundation, page speed greatly increased.",
+    type: "Client Work",
     demoLink: "http://calvinelandscape.com/",
   },
   {
-    name: "Let Me In",
+    title: "Let Me In",
     techStack: [
       "html5",
       "css3",
@@ -71,9 +81,11 @@ export const allProjects = [
       "express",
       "mongodb",
     ],
-    img: letMeInImg,
+    imgSrc: letMeInImg,
+    imgAlt: "",
     description:
       "A CRUD application that allows a user to add a home to a database with the corresponding gate/access code. The user will be able to search for the home when they're on route. The search mechanics are case-sensitive.",
+    type: "Personal",
     demoLink: "https://protected-oasis-33800.herokuapp.com/",
     repoLink:
       "https://github.com/sweetlikekendy/access-house-react-express-mongo",
