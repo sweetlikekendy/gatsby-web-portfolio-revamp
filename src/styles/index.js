5c51ade33e983fc027357704da85c2210a5f43bd
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-01-28 10:22:40 -0800

    remove button-link component

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,15 +1,7 @@
-import ButtonLink from "./button-link"
 import ExternalLink from "./external-link"
 import PrimaryButton from "./primary-button"
 import SecondaryButton from "./secondary-button"
 import Section from "./section"
 import Underline from "./underline"
 
-export {
-  ButtonLink,
-  ExternalLink,
-  PrimaryButton,
-  SecondaryButton,
-  Section,
-  Underline,
-}
+export { ExternalLink, PrimaryButton, SecondaryButton, Section, Underline }
