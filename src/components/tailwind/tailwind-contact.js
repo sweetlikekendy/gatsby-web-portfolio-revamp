61c360621f8e8a09d67236538b92659845a3645a
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-02-02 14:38:17 -0800

    add success and error alert for form

--- src/components/tailwind/tailwind-contact.js
+++ src/components/tailwind/tailwind-contact.js
@@ -1,46 +1,19 @@
 import React, { useState } from "react"
-import { navigate } from "gatsby"
 import { useForm } from "react-hook-form"
-import { AiOutlineMail } from "react-icons/ai"
+import {
+  AiFillCheckCircle,
+  AiFillCloseCircle,
+  AiOutlineClose,
+  AiOutlineMail,
+} from "react-icons/ai"
 import { PrimaryButton, StyledLink } from "../../styles"
-import DisabledButton from "../../styles/disabled-button"
 
 export default function Contact() {
-  // const [isSubmitting, setIsSubmitting] = useState(false)
-  // const [message, setMessage] = useState("Nothing to report!")
-  // const onSubmit = data => {
-  //   setIsSubmitting(true)
-  //   fetch("/.netlify/functions/contact", {
-  //     method: "POST",
-  //     body: JSON.stringify(data),
-  //   })
-  //     .then(response => response.json())
-  //     .then(response => {
-  //       setMessage(response.message)
-  //       setIsSubmitting(false)
-  //     })
-  //     .catch(error => {
-  //       // TODO test offline, remove console.log
-  //       // console.log({ error })
-  //       if (error.message === "Unexpected token < in JSON at position 0") {
-  //         setMessage("Uh oh! Something went wrong. Please try again later.")
-  //         setIsSubmitting(false)
-  //         return
-  //       }
-  //       if (error.message === "Failed to fetch") {
-  //         setMessage(
-  //           "Uh oh! Something went wrong. It may be your internet connection."
-  //         )
-  //         setIsSubmitting(false)
-  //         return
-  //       }
-  //       setMessage(error.message)
-  //       setIsSubmitting(false)
-  //     })
-  // }
-  const { register, handleSubmit, errors, formState, reset } = useForm()
+  const [message, setMessage] = useState("Nothing to report!")
+  const [isErrorAlert, setErrorAlert] = useState(false)
+  const [isSuccessAlert, setSuccessAlert] = useState(false)
 
-  const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+  const { register, handleSubmit, errors, formState, reset } = useForm()
 
   const encode = data => {
     return Object.keys(data)
@@ -49,19 +22,33 @@ export default function Contact() {
   }
 
   const onSubmit = (formData, event) => {
-    console.log("in onSubmit()", formData)
-    console.log(
-      "in onSubmit()",
-      encode({ "form-name": "contact-me", ...formData })
-    )
     fetch(`/`, {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact-me", ...formData }),
     })
       .then(response => {
-        navigate("/success/")
-        reset()
+        const { status } = response
+
+        switch (status) {
+          case 200:
+            setSuccessAlert(true)
+            setMessage(
+              "Thank you for reaching to me 😁 I will reply to your message ASAP."
+            )
+            reset()
+            break
+          case 400:
+            setErrorAlert(true)
+            setMessage(
+              "Hmm...unable to send your message. Double check the form and try again. If this error message shows up again, please try again later."
+            )
+            break
+          case 404:
+            setErrorAlert(true)
+            setMessage("The server cannot be found! Please try again later.")
+            break
+        }
         console.log(response)
       })
       .catch(error => {
@@ -76,7 +63,7 @@ export default function Contact() {
         <div className="absolute inset-y-0 left-0 w-1/2 bg-blueGray-50"></div>
       </div>
       <div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
-        <div className="flex items-center bg-blueGray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
+        <div className="flex items-center bg-blueGray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-16 xl:pr-12">
           <div className="max-w-lg mx-auto">
             <h2 className="text-2xl font-extrabold tracking-tight text-blueGray-900 sm:text-3xl">
               Ready to connect?
@@ -102,22 +89,64 @@ export default function Contact() {
             </dl>
           </div>
         </div>
-        <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
+        <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-16 lg:px-8 xl:pl-12">
           <div className="max-w-lg mx-auto lg:max-w-none">
             <form
-              action="/success"
+              // action="/success"
               method="POST"
               onSubmit={handleSubmit(onSubmit)}
               name="contact-me"
               data-netlify="true"
               data-netlify-honeypot="bot-field"
+              disabled={formState.isSubmitting}
             >
+              {isErrorAlert && (
+                <div className="rounded-md bg-red-50 p-4 mb-4">
+                  <div className="flex justify-between items-center">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0">
+                        <AiFillCloseCircle className="h-5 w-5 text-red-400" />
+                      </div>
+                      <div className="ml-3">
+                        <h3 className="text-sm font-medium text-red-800">
+                          {message}
+                        </h3>
+                      </div>
+                    </div>
+                    <div className="flex-shrink-0 flex items-center">
+                      <button onClick={() => setErrorAlert(!isErrorAlert)}>
+                        <AiOutlineClose className="h-5 w-5 text-red-800 " />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
+              {isSuccessAlert && (
+                <div className="rounded-md bg-green-50 p-4 mb-4">
+                  <div className="flex justify-between items-center">
+                    <div className="flex">
+                      <div className="flex-shrink-0">
+                        <AiFillCheckCircle className="h-5 w-5 text-green-400" />
+                      </div>
+                      <div className="ml-3">
+                        <h3 className="text-sm font-medium text-green-800">
+                          {/* Order completed */}
+                          {message}
+                        </h3>
+                      </div>
+                    </div>
+                    <div className="flex-shrink-0 flex items-center">
+                      <button onClick={() => setSuccessAlert(!isSuccessAlert)}>
+                        <AiOutlineClose className="h-5 w-5 text-green-800 " />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
               <fieldset
                 // disabled={isSubmitting || formState.submitCount >= 10}
                 className="grid grid-cols-1 gap-y-6"
               >
-                {/* <p>Message: {message}</p> */}
-                {/* <p>Submissions: {formState.submitCount}</p> */}
                 <div>
                   <label htmlFor="full_name" className="sr-only">
                     Full name
@@ -224,11 +253,6 @@ export default function Contact() {
                 <div>
                   <button type="submit">
                     <PrimaryButton>Submit</PrimaryButton>
-                    {/* {isSubmitting || formState.submitCount >= 10 ? (
-                      <DisabledButton>Submit</DisabledButton>
-                    ) : (
-                      <PrimaryButton>Submit</PrimaryButton>
-                    )} */}
                   </button>
                 </div>
               </fieldset>
