af2c7c813cb69a57a3afec5cbe26dadd7f00e721
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-02-03 10:44:08 -0800

    change title for seo on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import { FeatureSection } from "../components/tailwind"
 
 const IndexPage = () => (
   <Layout location="/">
-    <SEO title="Home" />
+    <SEO title="Kendy Nguyen's Portfolio" />
     <Hero />
     {/* <FeatureSection /> */}
     {/* <BrandAuthority /> */}
