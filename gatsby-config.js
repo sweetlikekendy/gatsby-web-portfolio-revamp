511f5dd47dc710dc4d7056ccd183a43a0486721d
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-02-03 11:59:21 -0800

    install 'gatsby-plugin-advanced-sitemap' plugin and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-advanced-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
