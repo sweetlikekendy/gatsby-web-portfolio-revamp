fcdc1a6e8060c78b351514c36e57e6c3d7f7e7ba
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-02-10 16:10:52 -0800

    add website status banner

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { useEffect } from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 import { Global, css } from "@emotion/core"
 import Footer from "./tailwind/footer"
 import Header from "./tailwind/header"
+import { CgClose } from "react-icons/cg"
 
 const Layout = ({ children, location }) => {
   // const data = useStaticQuery(graphql`
@@ -22,6 +23,9 @@ const Layout = ({ children, location }) => {
   //     }
   //   }
   // `)
+  const [showWebsiteStatusBanner, setShowWebsiteStatusBanner] = useState(true)
+
+  const handleWebsitStatusBanner = () => setShowWebsiteStatusBanner(false)
 
   return (
     <>
@@ -50,6 +54,25 @@ const Layout = ({ children, location }) => {
           }
         `}
       />
+      {showWebsiteStatusBanner && (
+        <div className="flex bg-blue-900 p-4 text-white uppercase text-center">
+          <div className="max-w-6xl flex mx-auto flex items-center">
+            <p className="text-sm">
+              If you notice anything not working or out of place, that is
+              because my website is currently undergoing some changes
+            </p>
+            <button
+              type="button"
+              stripHash
+              className="ml-3 inline-flex items-center p-2 rounded-md justify-center text-blueGray-100 hover:text-blueGray-700 hover:bg-blueGray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+              onClick={() => handleWebsitStatusBanner()}
+            >
+              <span className="sr-only">Close banner</span>
+              <CgClose className="w-5 h-5" />
+            </button>
+          </div>
+        </div>
+      )}
       {/* FLex in a column and have the main content grow to keep the footer always at the bottom of the page */}
       <div className="flex flex-col min-h-screen">
         <Header />
